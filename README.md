463f3507194fe2c2ad2044a5d15f4b187a03ba60
spencerwooo <spencer.woo@outlook.com>
2022-04-29 23:59:06 +0800

    add api support

--- README.md
+++ README.md
@@ -1,6 +1,39 @@
 # Web VPN Converter
 
-![screenshot](assets/screenshot.png)
+*Connect to your local networks in BIT from anywhere in the world.*
+
+## Demo
+
+[![screenshot](assets/screenshot.png)](https://webvpn.swo.moe)
+
+## API reference
+
+### Base
+
+```http
+GET /api/<ENCODED_URL>
+```
+
+Note that `ENCODED_URL` must be URL encoded.
+
+### Optional params
+
+* `prefix`: Must be one of `web` (default) or `lib`. Adds either `webvpn.bit.edu.cn` or `libvpn.bit.edu.cn` as prefix to the URL.
+* `redirect`: Either `true` or `false` (default). If `true`, the client will be redirected to the converted/encrypted URL directly.
+
+### Example
+
+```http
+GET /api/https%3A%2F%2Fbit.edu.cn?prefix=web&redirect=false
+```
+
+```json
+{
+  "url": "https://webvpn.bit.edu.cn/https/77726476706e69737468656265737421f2fe55d222347d1e7d06"
+}
+```
+
+## Development
 
 ```bash
 pnpm install
---
+++ pages/api/[url].ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { convert } from '../../lib/convert'
+
+const prefixOptions = { web: 'https://webvpn.bit.edu.cn', lib: 'https://libvpn.bit.edu.cn' } as const
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  // request like /api/https%3A%2F%2Fbit.edu.cn?prefix=web|lib&redirect=true|false
+  const { url, prefix = 'web', redirect = 'false' } = req.query
+
+  // if prefix is not one of web or lib, return invalid request
+  if (typeof prefix !== 'string' || !['web', 'lib'].includes(prefix)) {
+    res.status(400).json({ error: 'Invalid prefix' })
+    return
+  }
+  if (typeof url !== 'string') {
+    res.status(400).json({ error: 'Invalid url' })
+    return
+  }
+  if (typeof redirect !== 'string' || !['true', 'false'].includes(redirect)) {
+    res.status(400).json({ error: 'Invalid redirect option' })
+    return
+  }
+
+  const encryptedUrl = prefixOptions[prefix as keyof typeof prefixOptions] + convert(url)
+
+  // next.js query parameter does not parse boolean directly, so we have to do a string compare
+  if (redirect === 'true') {
+    res.redirect(302, encryptedUrl)
+  } else {
+    res.status(200).json({ url: encryptedUrl })
+  }
+}
